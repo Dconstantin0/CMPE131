299c6591b11b0d6d97f953a629ea3db2f4038cb0
ethan05d <ethan05dy@gmail.com>
2024-05-11 19:36:56 -0700

    updated database models and added comments

--- app/models.py
+++ app/models.py
@@ -3,17 +3,20 @@ from app import db
 # Model for our ticket DB
 class Ticket(db.Model):
     ticket_number = db.Column(db.Integer, primary_key=True)
-    flight_number = db.Column(db.Integer, db.ForeignKey('flight.flight_number'), nullable=False)
+    flight_number = db.Column(db.Integer, db.ForeignKey('flight.flight_number'))
     price = db.Column(db.Integer)
     seat_class = db.Column(db.String(10))
 
     flight = db.relationship('Flight', backref='tickets')
 
+# Model for our Route DB
 class Route(db.Model):
     route_id = db.Column(db.Integer, primary_key=True)
-    origin = db.Column(db.String(3))
+    # IATA Codes to determine
+    origin = db.Column(db.String(3)) 
     destination = db.Column(db.String(3))
 
+# Model for our Flight DB
 class Flight(db.Model):
     flight_number = db.Column(db.Integer, primary_key=True)
     route_id = db.Column(db.Integer, db.ForeignKey('route.route_id'))
