56d14419f42fad068987712dbb32e1f737676563
Wesley Zhao <tigerzhao999@gmail.com>
2024-05-15 14:35:58 -0700

    Added Ability to add airport to add page. updated add in routes.py

--- app/forms.py
+++ app/forms.py
@@ -8,16 +8,26 @@ class routeForm(FlaskForm):
     destination = StringField('Destination', validators=[DataRequired()])
     submit = SubmitField('Submit Route')
 
+
+class airportForm(FlaskForm):
+    airport_code = StringField('Airport IATA', validators=[DataRequired()])
+    airport_timezone = StringField('Airport Timezone', validators=[DataRequired()])
+    airport_terminal = StringField('Airport Terminal', validators=[DataRequired()])
+    airport_runways = StringField('Airport Runways', validators=[DataRequired()])
+    airport_url = StringField('Airport URL', validators=[DataRequired()])
+    submit = SubmitField('Submit Airport')
+
+
 # Flight form to add a new flight to the system
 class flightForm(FlaskForm):
     # Use these two fields to find Route ID
     flight_origin = StringField('Origin', validators=[DataRequired()])
     flight_destination = StringField('Destination', validators=[DataRequired()])
-
     flight_number = IntegerField('Flight #', validators=[DataRequired()])
     weather_description = StringField('Weather Description', validators=[DataRequired()])
     aircraft_model = StringField('Aircraft Model', validators=[DataRequired()])
     airline_name = StringField('Airline Name', validators=[DataRequired()])
     price = IntegerField('Price ($)', validators=[DataRequired()])
     submit = SubmitField('Submit Flight')
-    
+
+
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import url_for
 from flask import request
 
 from app import obj
-from app.forms import routeForm, flightForm
+from app.forms import routeForm, flightForm, airportForm
 
 from app.models import Ticket, Route, Flight, Airport
 from app import db
@@ -32,13 +32,13 @@ def flights_page():
         # For all flights corresponding to the route ID, add it to matching route origin
         for flight in Flight.query.filter_by(route_id=route.route_id).all():
             flight_info[route.origin].append(flight)
-        
+
     print(flight_info)
 
     return render_template("book_flights.html", flight_info=flight_info)
 
 # Route to book a flight and make a ticket
-@obj.route("/book-flight/<int:flight_number>", methods=["POST"])    
+@obj.route("/book-flight/<int:flight_number>", methods=["POST"])
 def book_flight(flight_number):
     if flight_number:
             # Get the flight the user clicked
@@ -85,30 +85,61 @@ def delete_booking(ticket_number):
     flash(f"Successful: Deleted Ticket #{ticket.ticket_number}, Flight #{ticket.flight_number}")
     return redirect(url_for("bookings_page"))
 
-# Route to add to airline routes and flights
+# Route to add to airline routes and flights and airports
 @obj.route("/add", methods=["GET", "POST"])
 def add_page():
-    # Create two add forms 
+    # Create two+1=3 add forms
     route_form = routeForm()
     flight_form = flightForm()
+    airport_form = airportForm()
+
+    # Check to see if user clicked the submit airport button
+    if airport_form.validate_on_submit():
+        #variables used to check and then to create
+        new_airport_code = airport_form.airport_code.data.upper()
+        new_terminal_info = airport_form.airport_terminal.data.upper() # Info text about terminals
+        new_timezone = airport_form.airport_timezone.data.upper() # Standard timezone in that area
+        new_runway_info = airport_form.airport_runways.data.upper() # Text about runways
+        new_url_link = airport_form.airport_url.data.upper()
+        print("helllo")
+        # Checks if airport name is IATA code or length of 3
+        if len(new_airport_code) != 3:
+            flash("Unsuccessful: Airport Code should be IATA Code (length of 3)")
+            return redirect("/add")
+        # checks if airport is already added by code
+        allairports = Airport.query.all()
+        for airport in allairports:
+            if airport.airport_code == new_airport_code:
+                flash("Unsuccessful: Duplicate airport detected")
+                return redirect("/add")
+        #otherwise create new airport in db
+        # Create a new airport in database
+        new_airport = Airport(airport_code=new_airport_code, terminal_info=new_terminal_info, timezone=new_timezone, runway_info=new_runway_info, url_link=new_url_link)
+        db.session.add(new_airport)
+        db.session.commit()
+        flash(f"Successful: New airport added #{new_airport.airport_code}")
+        return redirect("/add")
+
+
 
     # Check to see if user clicked the submit route button
     if route_form.validate_on_submit():
         new_origin = route_form.origin.data.upper()
         new_destination = route_form.destination.data.upper()
+        print("helllo route")
 
         # Checks if origin and destination are IATA codes or length of 3
         if (len(new_origin) != 3 or len(new_destination) != 3):
             flash("Unsuccessful: Origin and Destination should be IATA Codes (length of 3)")
             return redirect("/add")
-    
+
         # Check for duplicate records in database
         routes = Route.query.all()
         for route in routes:
             if route.origin == new_origin and route.destination == new_destination:
                 flash("Unsuccessful: Duplicate route detected")
                 return redirect("/add")
-            
+
         # Create a new route in database
         new_route = Route(origin=new_origin, destination=new_destination)
         db.session.add(new_route)
@@ -117,19 +148,20 @@ def add_page():
 
     # Check to see if user clicked the submit flight button
     if flight_form.validate_on_submit():
-        new_origin = flight_form.flight_origin.data.upper() 
+        new_origin = flight_form.flight_origin.data.upper()
         new_destination = flight_form.flight_destination.data.upper()
         new_flight_number = flight_form.flight_number.data
         new_weather_description = flight_form.weather_description.data
         new_aircraft_model = flight_form.aircraft_model.data.upper() # Used .upper() to fix formatting in DB
-        new_airline_name = flight_form.airline_name.data.upper() 
+        new_airline_name = flight_form.airline_name.data.upper()
         new_price = flight_form.price.data
+        print("hellloflight")
 
         # Checks if origin and destination are IATA codes or length of 3
         if (len(new_origin) != 3 or len(new_destination) != 3):
             flash("Unsuccessful: Origin and Destination should be IATA Codes (length of 3)")
             return redirect("/add")
-        
+
         # Check if there is an existing flight number already
         flights = Flight.query.all()
         for flight in flights:
@@ -150,11 +182,11 @@ def add_page():
                 db.session.commit()
                 flash("Successful: New route added")
                 return redirect("/add")
-        
+
         flash("Unsuccessful: Origin and destination not in the system")
         return redirect("/add")
-            
-    return render_template("add_page.html", route_form=route_form, flight_form=flight_form)
+
+    return render_template("add_page.html", route_form=route_form, flight_form=flight_form, airport_form=airport_form)
 
 
 
